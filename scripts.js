bd9fb9c29325c84cf5538bb99ad7adff73e44d3f
Magdalena Michalska <magda@MacBook-Pro-Magdalena.local>
2017-06-05 21:52:12 +0200

    add scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+var newName = 'Marian';
+
+if(allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
+} else {
+	console.log("To imię jest już na tablicy");
+};
+
+console.log(allNames);
